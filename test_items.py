4984caa7fe6e9bbf795fb3313648353c7c410520
ButerBrodsky <122352068+ButerBrodsky@users.noreply.github.com>
2023-11-26 20:46:50 +0300

    Прочти дескрипшн
    Гайз, кто будет это проверять, я не очень понял, надо ли, чтобы язык действительно менялся (es \ ru), но я добавил само наличие этого опшна, хз, насколько выполнена эта часть сделки, поэтому заранее сорян. В условиях задачи не вижу пункта, чтобы страничка с книгой действительно открывалась на испанском

--- test_items.py
+++ test_items.py
@@ -1,1 +1,12 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import pytest
 
+def test_exercise(browser):
+    link = 'https://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    browser.get(link)
+    button_submit = browser.find_element(By.CSS_SELECTOR, 'button.btn.btn-lg.btn-primary.btn-add-to-basket').text
+    assert "Добавить в корзину" == button_submit, ("Кнопки добавления в корзину нет!")
+    time.sleep(3)
